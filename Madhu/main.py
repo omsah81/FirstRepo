50e6e2f8f2ba3a894503e8398336eea46b32a354
omsah <huncyoms@gmail.com>
2023-04-24 05:45:43 -0400

    commit

---
+++ Madhu/main.py
@@ -0,0 +1,18 @@
+def claculategmean(a,b):
+    gmean = (a+b)/(a*b)
+    print(gmean)
+
+def isgreater(a,b):
+    if(a>b):
+        print("a is greater value:")
+    else:
+        print("b is greater value: ")
+
+a = 10
+b =12
+isgreater(a,b)
+claculategmean(a,b)
+c =13
+d =20
+isgreater(c,d)
+claculategmean(c,d)
